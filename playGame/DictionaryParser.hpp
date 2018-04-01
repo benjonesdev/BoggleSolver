e06872732796e9349eeb3823aa6becd7d83255b6
Alice-Yan <lifyan1982@gmail.com>
2018-04-01 09:51:59 -0500

    add foundWords function to play game

---
+++ playGame/DictionaryParser.hpp
@@ -0,0 +1,22 @@
+/****************************************
+ * Dictionary Reader Class Header File *
+*****************************************/
+
+#ifndef DICTIONARY_PARSER_HPP
+#define DICTIONARY_PARSER_HPP
+
+#include <string>
+#include <unordered_map>
+
+
+class DictionaryParser
+{
+private:
+    std::unordered_map<std::string, int> validWords;
+public:
+    void parseTextFile(std::string); // parse text file
+    std::unordered_map<std::string, int> getValidWords(); // returns valid words read from text file as a hash map
+};
+
+
+#endif
---
+++ playGame/validation.hpp
@@ -0,0 +1,17 @@
+/****************************************
+ * function header file
+*****************************************/
+
+#ifndef VALIDATION_HPP
+#define VALIDATION_HPP
+#include <iostream>
+
+//void onCorner(int, int);
+
+//void EdgeMiddle(int, int);
+
+int validation(int min, int max);
+
+char validationChar();
+
+#endif
